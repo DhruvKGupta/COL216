de77f4e9ab466f0fe0e24ecf502c8568df2e4700
SwarajGawande <cs1190406@iitd.ac.in>
2021-03-01 22:36:00 +0530

    reduced number of registers

--- Assignment2/Assignment2.asm
+++ Assignment2/Assignment2.asm
@@ -33,33 +33,32 @@ main:
 
 	li $v0, 8 		# call code, read string
 	la $a0, input 		# addr, where to put chars
-	li $a1, 128 		# max chars for string
+	li $a1, 128		# max chars for string
 	syscall
 
 	li $t0, 0 		#index of character of string, loop index
 	li $t1, 0		#Size of stack currently, will be used to check for validity of push
 
 	la $s0, input 		#load string to s0
-	li $t2, '\n'
+	#li $t2, '\n'
 
-#Thoda sa starting ka likha..ab bas har charcter dekhna hai..agar 0-9 hai to push, agar +,-,* hai to kuch kuch krna hai..warna error
 readLoop:
 	lb $s2, 0($s0)		#loading one character at a time as a byte
 
-	beq $s2,10,endloop	#checking if we reached end of line 
-	beq $s2,$t2,endloop
+	beq $s2,10,endloop	#checking for newline
+	beq $s2,0,endloop 	#checking for null 
 
-	bge $s2, 48, variable
-	beq $s2, 42, multiplication
-	beq $s2, 43, addition
-	beq $s2, 45, substraction
-	j error1
+	bge $s2, 48, variable	#if the character is having ascii value greater than 0
+	beq $s2, 42, multiplication	#if char is *
+	beq $s2, 43, addition	#if char is +
+	beq $s2, 45, substraction	#if char is -
+	j error1		# if the character is none of the above then error invalid character
 	
 variable:
-	bgt $s2, 57, error1	#not a valid character
-	addi $s2,$s2,-48
+	bgt $s2, 57, error1	#not a valid character not in 0-9
+	addi $s2,$s2,-48	#calculating exact value of the number from character
 
-	subu $sp, $sp, 4 # push array[i]
+	subu $sp, $sp, 4 	# push array[i]
 	sw $s2, ($sp)
 	addi $t1,$t1,1
 
@@ -67,64 +66,61 @@ variable:
 	
 multiplication:
 
-	slti $t5,$t2,2
-	bne $t5,$zero,error2
+	blt $t1, 2, error2
 	
 	lw $s4, ($sp)
-	lw $s3, 4($sp)
-	addu $sp, $sp, 8
+	addu $sp, $sp, 4
+	lw $s3, ($sp)
+
 
 	mul $s3,$s3,$s4
 
-	subu $sp, $sp, 4 # push array[i]
+	#subu $sp, $sp, 4 	# push array[i]
 	sw $s3, ($sp)
 	addi $t1,$t1,-1
 
 	j increment		#skip to increment index and proceed
 	
 addition:
-	slti $t5,$t2,2
-	bne $t5,$zero,error2
+	blt $t1,2,error2	#if we cannot pop 2 numbers from the stack
 	
-	lw $s4, ($sp)
-	lw $s3, 4($sp)
-	addu $sp, $sp, 8
+	lw $s4, ($sp)		#loading two numbers in s4 and s3
+	addu $sp, $sp, 4
+	lw $s3, ($sp)
 
-	add $s3,$s3,$s4
+	add $s3,$s3,$s4		#performing addition of these two numbers
 
-	subu $sp, $sp, 4 # push array[i]
-	sw $s3, ($sp)
-	addi $t1,$t1,-1
+	#subu $sp, $sp, 4 	# push array[i]
+	sw $s3, ($sp)		#set the new value after multiplication
+	addi $t1,$t1,-1		#reduce the count of numbers in stack by 1
 
 	j increment		#skip to increment index and proceed
 
 substraction:
-	slti $t5,$t2,2
-	bne $t5,$zero,error2
+	blt $t1,2,error2
 	
 	lw $s4, ($sp)
-	lw $s3, 4($sp)
-	addu $sp, $sp, 8
+	addu $sp, $sp, 4
+	lw $s3, ($sp)
 
 	sub $s3,$s3,$s4
 
-	subu $sp, $sp, 4 # push array[i]
+	#subu $sp, $sp, 4 	# push array[i]
 	sw $s3, ($sp)
 	addi $t1,$t1,-1
 
 increment:	
-	addi $s0,$s0,1	
+	addi $s0,$s0,1		#increment the index of char in string
 	j readLoop
 
 endloop:
-	li	$s7, 1		# $s7 =1 
-	bne	$t1, $s7, error2	# if size not 1
+	bne $t1, 1, error2	# if size not 1
 
 	lw $s4, ($sp)
 	addu $sp, $sp, 4
 	
-	la $a0, finalans		#code to print string finalans
-	li $v0 4
+	la $a0, finalans	#code to print string finalans
+	li $v0, 4
 	syscall			#syscall to print the string
 
 	li $v0,1
