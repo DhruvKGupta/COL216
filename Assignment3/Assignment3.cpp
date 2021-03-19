70dfeba28946a873890aabc4395ac78e62e7f49e
SwarajGawande <cs1190406@iitd.ac.in>
2021-03-19 20:24:18 +0530

    before minor changes

--- Assignment3/Assignment3.cpp
+++ Assignment3/Assignment3.cpp
@@ -440,69 +440,69 @@ private:
 
     int isvalid(string name)
     {
-        if (name == "$0" || name == "$zero")
+        if (name == "$r0" || name == "$zero")
             return 0;
-        else if (name == "$1" || name == "$r0")
+        else if (name == "$at" || name == "$r0")
             return 1;
-        else if (name == "$2" || name == "$r1")
+        else if (name == "$v0" || name == "$r1")
             return 2;
-        else if (name == "$3" || name == "$r2")
+        else if (name == "$v1" || name == "$r2")
             return 3;
-        else if (name == "$4" || name == "$r3")
+        else if (name == "$a0" || name == "$r3")
             return 4;
-        else if (name == "$5" || name == "$r4")
+        else if (name == "$a1" || name == "$r4")
             return 5;
-        else if (name == "$6" || name == "$r5")
+        else if (name == "$a2" || name == "$r5")
             return 6;
-        else if (name == "$7" || name == "$r6")
+        else if (name == "$a3" || name == "$r6")
             return 7;
-        else if (name == "$8" || name == "$r7")
+        else if (name == "$t0" || name == "$r7")
             return 8;
-        else if (name == "$9" || name == "$r8")
+        else if (name == "$t1" || name == "$r8")
             return 9;
-        else if (name == "$10" || name == "$r9")
+        else if (name == "$t2" || name == "$r9")
             return 10;
-        else if (name == "$11" || name == "$r10")
+        else if (name == "$t3" || name == "$r10")
             return 11;
-        else if (name == "$12" || name == "$r11")
+        else if (name == "$t4" || name == "$r11")
             return 12;
-        else if (name == "$13" || name == "$r12")
+        else if (name == "$t5" || name == "$r12")
             return 13;
-        else if (name == "$14" || name == "$r13")
+        else if (name == "$t6" || name == "$r13")
             return 14;
-        else if (name == "$15" || name == "$r14")
+        else if (name == "$t7" || name == "$r14")
             return 15;
-        else if (name == "$16" || name == "$r15")
+        else if (name == "$s0" || name == "$r15")
             return 16;
-        else if (name == "$17" || name == "$r16")
+        else if (name == "$s1" || name == "$r16")
             return 17;
-        else if (name == "$18" || name == "$r17")
+        else if (name == "$s2" || name == "$r17")
             return 18;
-        else if (name == "$19" || name == "$r18")
+        else if (name == "$s3" || name == "$r18")
             return 19;
-        else if (name == "$20" || name == "$r19")
+        else if (name == "$s4" || name == "$r19")
             return 20;
-        else if (name == "$21" || name == "$r20")
+        else if (name == "$s5" || name == "$r20")
             return 21;
-        else if (name == "$22" || name == "$r21")
+        else if (name == "$s6" || name == "$r21")
             return 22;
-        else if (name == "$23" || name == "$r22")
+        else if (name == "$s7" || name == "$r22")
             return 23;
-        else if (name == "$24" || name == "$r23")
+        else if (name == "$t8" || name == "$r23")
             return 24;
-        else if (name == "$25" || name == "$r24")
+        else if (name == "$t9" || name == "$r24")
             return 25;
-        else if (name == "$26" || name == "$r25")
+        else if (name == "$k0" || name == "$r25")
             return 26;
-        else if (name == "$27" || name == "$r26")
+        else if (name == "$k1" || name == "$r26")
             return 27;
-        else if (name == "$28" || name == "$r27")
+        else if (name == "$gp" || name == "$r27")
             return 28;
-        else if (name == "$29" || name == "$r28")
+        else if (name == "$sp" || name == "$r28")
             return 29;
-        else if (name == "$30" || name == "$r29")
+        else if (name == "$s8" || name == "$r29")
             return 30;
-        else if (name == "$31" || name == "$sp")
+        else if (name == "$ra" || name == "$r30")
             return 31;
         else
             return -1;
@@ -842,8 +842,29 @@ private:
             i = operands.find('(');
             if (i == -1)
             {
-                cout << "invalid instruction at line ";
-                return false;
+            	int j=operands.length() - 1;
+            	while (j>=0 && (operands[j] == ' ' || operands[j] == '\t'))
+            	{
+                j++;
+            	}
+                string reg = operands.substr(0, j + 1);
+                if (checkNum(reg))
+                {
+                    Instruction instr;
+            	instr.instr = oper;
+            	instr.imvalue = stoi(reg);
+            	instr.dest = reg1;
+            	instr.src1 = 0;
+            	instr.src2 = -1;
+            	instructions.push_back(instr);
+            	return true;;
+                }
+                else
+                {
+                    cout << " Syntax Error at line ";
+                    return false;
+                }
+                return true;
             }
             j = i - 1;
             while (j >= 0 && (operands[j] == ' ' || operands[j] == '\t'))
